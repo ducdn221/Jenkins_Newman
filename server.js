a5af353cecbc3d2ede1d202451ac07bc2c57723e
dnduc <dnduc@cmc.com.vn>
2019-12-19 15:20:18 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const addRequestId = require("express-request-id");
+
+const app = express();
+app.use(addRequestId());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello world", requestId: req.id });
+});
+
+app.get("/foo", ({ id, query }, res, next) => {
+  const { bar } = query;
+  res.json({message: "successfull", bar: `${bar}`, requestId: id });
+});
+
+app.post("/foo", ({ id, body }, res, next) => {
+  const { bar } = body;
+
+  if (typeof bar === "undefined") {
+    return res.status(400).json({ error: "missing `bar`", requestId: id });
+  }
+
+  res.json({message: "successfull", bar: `${bar}`, requestId: id });
+});
+
+
+const server = app.listen(8081, function() {
+  const port = server.address().port;
+  console.log("Example app listening to port %s", port);
+});
