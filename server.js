32ccf060c6d0d4c3b7c0af360852dcfc17f03c16
dnduc <dnduc@cmc.com.vn>
2019-12-20 10:50:57 +0700

    1.Authen with jwt, 2.Update postman collection

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
+require("rootpath")();
 const express = require("express");
 const bodyParser = require("body-parser");
 const addRequestId = require("express-request-id");
+const cors = require("cors");
+const jwt = require("_helpers/jwt");
+const errorHandler = require("_helpers/error-handler");
 
 const app = express();
 app.use(addRequestId());
 app.use(bodyParser.json());
+app.use(cors());
+
+//use JWT auth to seure the api
+
+app.use(jwt());
+
+//api routes
+app.use('/users',require('./users/users.controller'));
+
+//global error handler
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.json({ message: "hello world", requestId: req.id });
@@ -12,7 +27,7 @@ app.get("/", (req, res) => {
 
 app.get("/foo", ({ id, query }, res, next) => {
   const { bar } = query;
-  res.json({message: "successfull", bar: `${bar}`, requestId: id });
+  res.json({ message: "successfull", bar: `${bar}`, requestId: id });
 });
 
 app.post("/foo", ({ id, body }, res, next) => {
@@ -22,9 +37,8 @@ app.post("/foo", ({ id, body }, res, next) => {
     return res.status(400).json({ error: "missing `bar`", requestId: id });
   }
 
-  res.json({message: "successfull", bar: `${bar}`, requestId: id });
+  res.json({ message: "successfull", bar: `${bar}`, requestId: id });
 });
-
 
 const server = app.listen(8081, function() {
   const port = server.address().port;
